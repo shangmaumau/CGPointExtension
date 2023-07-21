6419c0af14d51cccd2d43b6de0b1d4decab4dcba
尚毛毛 <shangmaumau@live.com>
2023-07-21 16:06:23 +0800

    first commit

--- Sources/CGPointExtension/CGPointExtension.swift
+++ Sources/CGPointExtension/CGPointExtension.swift
@@ -1,2 +1,14 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+import CoreFoundation
+
+public let version = "1.0.0"
+
+extension CGPoint {
+    
+    public init(_ x: CGFloat, _ y: CGFloat) {
+        self.init(x: x, y: y)
+    }
+}
